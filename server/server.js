f184d7ca429532f81226fc4d58dc8d058e4c5fc9
isaiah delgado <isaiah_del@yahoo.com>
2021-10-02 11:51:18 -0400

    changed global error handler and changed conditional for production mode serving

--- server/server.js
+++ server/server.js
@@ -11,26 +11,21 @@ if (process.env.NODE_ENV === 'production') {
   // statically serve everything in the build folder on the route '/build'
   app.use('/', express.static(path.join(__dirname, '../build')));
   
-  // serve index.html on the route '/'
-  app.get('/', (req, res) => {
-    return res.status(200).sendFile(path.resolve('build', './index.html'));
-  });
 }
 
 //catch-all route handler
-app.use((req, res) => res.status(404).send('Oh no!! Page not found'));
+// app.use((req, res) => res.status(404).send('Oh no!! Page not found'));
 
 
 //global error handler
 app.use((err, req, res, next) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+  console.error(err);
+  const defaultClientError = {
     status: 500,
-    message: { err: 'An error occurred' },
+    message: { error: 'An unexpected error occurred.' },
   };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
+  const errorObj = Object.assign(defaultClientError, err);
   return res.status(errorObj.status).json(errorObj.message);
 })
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
