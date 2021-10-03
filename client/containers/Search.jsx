1f38bd197ba721f34e55904a72a48cf6a440e6bb
Justin Stoddard <justinstoddard@Justins-MacBook-Air.local>
2021-10-02 17:03:27 -0700

    Search container, Artist view, FollowButton view setup, awaiting back end functionality

--- client/containers/Search.jsx
+++ client/containers/Search.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './../views/NavBar.jsx';
 import Artist from './../views/Artist.jsx'
 import NoArtist from './../views/NoArtist.jsx';
+import Splash from './../views/Splash.jsx'
 import './../sass/containers/Search.scss';
 
 
@@ -15,18 +16,33 @@ export default function Search({
 
 const [artists, setArtists] = useState(undefined);
 
+/* TODO useEffect queries API, creates aritstInfo object */
 
 return (
   <div id="search">
       <NavBar searchValue={searchValue} setSearchValue={setSearchValue}/>
-    {artists && artists.length === 0 ? <NoArtist/> : artists.map(artistInfo => {
-        <Artist></Artist>
-    }  )}
+    {artists === undefined && <Splash/>}
+    {artists && artists.length === 0 
+      ? <NoArtist/> 
+      : artists.map(artistInfo => {
+          <Artist
+            key={artistInfo.artistName}
+            artistDes={artistInfo.artistDes}
+            artistImageURL={artistInfo.artistImageURL}
+            artistName={artistInfo.artistName}
+            artistOnTour={artistInfo.artistOnTour}
+            isFollowed={followedArtists.find(artistInfo.artistId) !== undefined}
+            addArtist={addArtist}
+            artistId={artistInfo.artistId}
+            removeArtist={removeArtist}
+          ></Artist>
+      })
+    }
 
 
   </div>
 )
 
-};
+}
 
 
--- client/views/Artist.jsx
+++ client/views/Artist.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
-
+import FollowButton from './FollowButton.jsx'
+import UnfollowButton from './UnfollowButton.jsx'
 import './../sass/views/Artist.scss';
 
-export default function Artist()
+export default function Artist({
+    addArtist,
+    artistId,
+    artistDes,
+    artistImageURL,
+    artistName,
+    artistOnTour,
+    isFollowed,
+    removeArtist
+  }) {
+
+  return (
+    <article id="artist">
+      <header>
+        <img src={artistImageURL}alt={`Event image for ${artistName}`}></img>
+        </header>
+        <h3>{artistName}</h3>
+        <div id="artist-des">
+          <p>{artistDes}</p>
+        </div>
+        {isFollowed && <UnfollowButton onClick={addArtist(artistId)}/> }
+        {!isFollowed && <FollowButton onClick={removeArtist(artistId)}/>}
+        
+       </article> 
+
+  )
+} 
+
 
