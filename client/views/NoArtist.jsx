d6cdb4a2b978fc55a8acdc5ef1ad0fd626513a61
Miguel Hernandez <miguelh72@outlook.com>
2021-10-02 21:11:40 -0400

    conforming file format of NoArtist component
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- client/views/NoArtist.jsx
+++ client/views/NoArtist.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import './../sass/views/NoArtist.jsx'
+import './../sass/views/NoArtist.jsx';
 
 export default function NoArtist() {
   return (
-      <div id="no-artist">
-          <h2> No results found </h2>
-          <p>Sorry, try again!</p>
-      </div>
-  )
-
+    <div id="no-artist">
+      <h2> No results found </h2>
+      <p>Sorry, try again!</p>
+    </div>
+  );
 }
