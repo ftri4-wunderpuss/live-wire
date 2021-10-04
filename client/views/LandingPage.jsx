1e28b43e3cc893e232456aabee54b0cb58402c69
Miguel Hernandez <miguelh72@outlook.com>
2021-10-04 13:09:09 -0400

    Added styling for landing page  feature and fixed its positioning on page resizes
    Co-authored-by: jgstoddard <jgstoddard@users.noreply.github.com>
    Signed-off-by: Miguel Hernandez <miguelh72@outlook.com>

--- client/views/LandingPage.jsx
+++ client/views/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Paper from '@mui/material/Paper';
 
 import './../sass/views/LandingPage.scss';
 
@@ -9,15 +10,26 @@ export default function LandingPage({
   openSignUpModal,
 }) {
   return (
-    <div id="landing-page">
+    <>
       <GuestNavBar
         openLoginModal={openLoginModal}
         openSignUpModal={openSignUpModal}
       />
-      <div id="promo-text">
-        {/* TODO add promo text */}
-        <p> TODO: PROMO TEXT </p>
+      <div id="landing-page" >
+        <Paper
+          id="promo-text"
+          elevation={5}
+        >
+          {/* TODO add promo text */}
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Paper>
       </div>
-    </div>
+    </>
   );
 }
